71d86e126c4be55a5fcb82363eb1aa04ca2bc9ea
RiCK BROWN <ricky.brown.00@gmail.com>
2018-03-22 23:19:11 +0000

    Corrected Master Branch

--- free-code-camp-algorthyms/app.js
+++ free-code-camp-algorthyms/app.js
@@ -6,43 +6,10 @@ import { fail } from "assert";
 // Can easily write FCC Algorithms.
 
 // QUESTION
-// Check if the predicate(second argument) is truthy on all elements of a collection(first argument).
-// Remember, you can access object properties through either dot notation or[] notation.
+
 
 
 // RETURNS
-// truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex") should return true.
-// truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex") should return false.
-// truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0}, {"user": "Dipsy", "sex": "male", "age": 3}, {"user": "Laa-Laa", "sex": "female", "age": 5}, {"user": "Po", "sex": "female", "age": 4}], "age") should return false.
-// truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true}, {"name": "FastFoward", "onBoat": null}], "onBoat") should return false
-// truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true, "alias": "Repete"}, {"name": "FastFoward", "onBoat": true}], "onBoat") should return true
-// truthCheck([{"single": "yes"}], "single") should return true
-// truthCheck([{"single": ""}, {"single": "double"}], "single") should return false
-// truthCheck([{"single": "double"}, {"single": undefined}], "single") should return false
-// truthCheck([{"single": "double"}, {"single": NaN}], "single") should return false
+
 
 // MY FUNCTION
-function truthCheck(collection, pre) {
-    let counter = 0;
-    // Is everyone being true?
-    for (let i = 0; i < collection.length; i++) {
-        console.log(collection[i]);
-        if (collection[i].hasOwnProperty(pre)) {
-            console.log(collection[i] + ": TRUE");
-            counter += 1;
-        } else {
-            console.log(collection[i] + ": FALSE");
-        }
-    }
-
-    if (counter === collection.length) {
-        console.log("** TRUE **");
-        return true;
-    } else {
-        console.log("** FALSE **");
-        return false;
-    }
-}
-
-truthCheck([{ "user": "Tinky-Winky", "sex": "male" }, { "user": "Dipsy", "sex": "male" }, { "user": "Laa-Laa", "sex": "female" }, { "user": "Po", "sex": "female" }], "sex")
-// should return true
